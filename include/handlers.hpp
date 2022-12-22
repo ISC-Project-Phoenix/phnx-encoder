63db2913f9465a920ef17c6cb8c4846f9d4e4e43
nicobokha <nicobokha@gmail.com>
2022-12-22 14:31:32 -0500

    add attrs and methods for calcVel

--- include/handlers.hpp
+++ include/handlers.hpp
@@ -10,10 +10,12 @@ class Encoder {
     uint8_t chanA;
     uint8_t chanB;
     int64_t counter{};
+    int64_t prevCounter{};
+    const int64_t TICKS_PER_REV;
     Direction dir;
 
 public:
-    Encoder(uint8_t chanA, uint8_t chanB) : chanA(chanA), chanB(chanB), dir(Direction::CW) {}
+    Encoder(uint8_t chanA, uint8_t chanB) : chanA(chanA), chanB(chanB), dir(Direction::CW), TICKS_PER_REV(600){}
 
     int64_t tick() {
         auto aState = digitalRead(chanA);
@@ -22,12 +24,19 @@ public:
         if (digitalRead(chanB) != aState) {
             counter--; //Encoder is moved one Tick in CCW direction
             dir = Direction::CCW;
-        } else {//Otherwise, Encoder moved one Tick in CW direction
+        } else {//Otherwise, Encoder moved ointellne Tick in CW direction
             counter++;
             dir = Direction::CW;
         }
 
         return counter;
+    }
+
+    float calcVel(){
+        int64_t displacement = counter - prevCounter;
+        int64_t enc_rev = displacement / TICKS_PER_REV;
+        // enc_rev * 
+        return 0;
     }
 
     Direction getDir() {
